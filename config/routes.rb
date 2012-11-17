30a2f07ab9bc6c249fa13a37362637e6a155580a
Dan Francis <danfrancis707@gmail.com>
2012-11-17 09:22:31 -0800

    Add StaticPAges controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 FreundFootball::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/faq"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
