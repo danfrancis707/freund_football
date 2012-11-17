2f1b7027c232381dfdad070f4a5d986b42df5770
Dan Francis <danfrancis707@gmail.com>
2012-11-16 20:56:27 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreundFootball::Application.config.secret_token = '3350231146f3815fb52751c1021ec330f62532c5b2ee35a6c343b3345d43439ae06f31ce51b4de9b4f48b6ace8765333c838e5d6b39df5ffaed824505614dd77'
