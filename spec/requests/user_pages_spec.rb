9890b8b9e287ce1d3b6ac1303c1224a15d568dbd
Dan Francis <danfrancis707@gmail.com>
2012-11-18 15:53:21 -0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,7 +12,9 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
   end
 
-  before { visit signup_path }
+  describe "signup" do  
+    
+    before { visit signup_path }
 
     let(:submit) { "Create my account" }
 
@@ -30,9 +32,19 @@ describe "User pages" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
   end
 end
+
