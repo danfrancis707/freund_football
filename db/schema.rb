b3aec18c1c84ef3c588ac817d6a86981f6708283
Dan Francis <danfrancis707@gmail.com>
2012-11-19 21:31:33 -0800

    added address column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121119013451) do
+ActiveRecord::Schema.define(:version => 20121120043517) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20121119013451) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.string   "phone"
+    t.string   "address"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
